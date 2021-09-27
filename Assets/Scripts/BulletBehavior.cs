b805d5223e87a420f0edac33039508a6bc1f3ee9
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2021-09-26 22:42:52 -0700

    Ricochet
    Tried to implement the code. refer to the comment.

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
+    public LayerMask collisionMask;
     public float bulletSpeed;
     public int ricochets;
 
@@ -11,8 +12,21 @@ public class BulletBehavior : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * bulletSpeed);
-        
-        
+
+
         //Destroy(gameObject, 3f);
+
+        //Ricocheting code, having difficulty, I think the bullets themselves need to be an individual object so we can 
+        //apply a collider? Right now its only going through the walls. 
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Time.deltaTime * bulletSpeed + .1f,collisionMask))
+        {
+            Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
+            float rot = 90 - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rot, 0);
+        }
+
+
     }
 }
