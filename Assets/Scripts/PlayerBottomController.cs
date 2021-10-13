975f694506e69c7820365fef9c707585862f79c9
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2021-10-13 09:05:43 -0700

    Added rotation based on movement
    Changed up the movement script to make it rotate based on the current direction

--- Assets/Scripts/PlayerBottomController.cs
+++ Assets/Scripts/PlayerBottomController.cs
@@ -6,55 +6,25 @@ public class PlayerBottomController : MonoBehaviour
 {
     //Variables
     public float movementSpeed;
-    public GameObject camera;
-    //public RigidBody rb;
+    public float rotationSpeed;
 
     //functions
     void Update()
     {
-        //Player facing mouse
-        Plane playerPlane = new Plane(Vector3.up, transform.position);
-        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
-        float hitDist = 0.0f;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        //float characterAngle = Vector3.Angle(Vector3.forward, transform.forward);
-        //Quaternion rotation = Quaternion.AngleAxis(characterAngle, Vector3.up);
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
 
-        //not good because axis gets changed locally
-        /*if(playerPlane.Raycast(ray, out hitDist))
-        {
-            Vector3 targetPoint = ray.GetPoint(hitDist);
-            //Vector3 relativePos = Vector3.magnitude - transform.position;
-            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-            //Quaternion targetRotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        transform.Translate(movementDirection * movementSpeed * Time.deltaTime, Space.World);
 
-            targetRotation.x = 0;
-            targetRotation.z = 0;
+        //makes the rotation based on current direction
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
-            //transform.rotation = targetRotation;
-        }*/
-
-        //PlayerMovement
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
-            //transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime * movementSpeed, Space.Self);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
-            //transform.Rotate(new Vector3(0, -10, 0) * Time.deltaTime * movementSpeed, Space.Self);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
-            //transform.Rotate(new Vector3(0, -10, 0) * Time.deltaTime * movementSpeed, Space.Self);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
-            //transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime * movementSpeed, Space.Self);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
     }
 }
