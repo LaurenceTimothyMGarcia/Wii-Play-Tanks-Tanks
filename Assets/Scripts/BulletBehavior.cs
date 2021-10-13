d354fe53378cff73a6433f3b0369bc9320825581
EmergencyPlayer <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2021-10-13 15:10:19 -0700

    trying to work on ricochet

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -6,13 +6,25 @@ public class BulletBehavior : MonoBehaviour
 {
     public float bulletSpeed;
     public int ricochets;
+    public Transform bulletTransform;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * bulletSpeed);
+        bulletTransform = transform.Translate(Vector3.forward * Time.deltaTime * bulletSpeed);
         
         
         //Destroy(gameObject, 3f);
     }
+
+    /*void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.CompareTag("Block"))
+        {
+            Vector3 wallNormal = coll.contacts[0].normal;
+            bulletTransform = Vector3.Reflect(bulletTransform.position, wallNormal).normalize;
+
+            bulletTransform.velocity = bulletTransform * bulletSpeed;
+        }
+    }*/
 }
