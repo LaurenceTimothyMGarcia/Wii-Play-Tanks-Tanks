6098a5256daf44b9650221d02338ea5d6015de79
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2021-10-12 22:56:40 -0700

    edited out
    edited out just in case

--- Assets/Scripts/RotateBot.cs
+++ Assets/Scripts/RotateBot.cs
@@ -9,22 +9,21 @@ public class RotateBot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 rotation = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+            //transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime * rotationSpeed, Space.Self);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+            //transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime * rotationSpeed, Space.Self);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+            //transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime * rotationSpeed, Space.Self);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime * rotationSpeed, Space.Self);
+            //transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime * rotationSpeed, Space.Self);
         }
     }
 }
