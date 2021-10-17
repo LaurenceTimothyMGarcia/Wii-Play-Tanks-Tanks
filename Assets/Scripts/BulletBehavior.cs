3f4c3a1b4f521941ed91ec3f5bd8a0587c9e7e8f
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2021-10-17 15:23:18 -0700

    Trying bullets
    bullet behavior

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -4,6 +4,132 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
+    public GameObject bullet;
+
+    public float timeBetweenShooting, spread, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+
+
+    int bulletsLeft, bulletShoot;
+    bool shooting, readyToSchoot, reloading;
+
+    public Camera fpsCam;
+    public Transform attackPoint;
+
+    //graphics
+    public GameObject muzzuleFlash;
+    
+
+    public bool allowInvoke;
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToSchoot = true;
+    }
+
+    private void Update()
+    {
+        myInput();
+    }
+
+    private void myInput()
+    {
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        //reloading
+        if (readyToSchoot && shooting && !reloading && bulletsLeft <= 0)
+        {
+            Reload();
+        }
+
+        //shooting
+        if (readyToSchoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletShoot = 0;
+
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToSchoot = false;
+
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+
+        }
+
+        else
+        {
+            targetPoint = ray.GetPoint(75);
+        }
+
+        Vector3 direction = targetPoint - attackPoint.position;
+
+        //spawed bullets are stored here
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+
+        if (muzzuleFlash != null)
+        {
+            Instantiate(mazzuleFlash, attackPoint.position, Quaternion);
+        }
+        bulletsLeft--;
+        bulletShoot++;
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+    }
+
+
+
+
+    private void ResetShot()
+    {
+        readyToSchoot = true;
+        allowInvoke = true;
+
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished".Insert reloadTime);
+    }
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* previous code
+
     public LayerMask collisionMask;
     public float bulletSpeed;
     public int ricochets;
@@ -27,6 +153,4 @@ public class BulletBehavior : MonoBehaviour
             transform.eulerAngles = new Vector3(0, rot, 0);
         }
 
-
-    }
-}
+*/
