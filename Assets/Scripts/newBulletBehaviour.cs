1747fc9931db1850c7e8e7d04d0e596095bd7ddf
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2021-10-25 03:14:23 -0700

    Testing bullets
    no big changes

---
+++ Assets/Scripts/newBulletBehaviour.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newBulletBehaviour : MonoBehaviour
+{
+    public Transform originalObject;
+    public Transform reflectedObject;
+
+    void Update()
+    {
+        // Makes the reflected object appear opposite of the original object,
+        // mirrored along the z-axis of the world
+        reflectedObject.position = Vector3.Reflect(originalObject.position, Vector3.right);
+    }
+}
